491f675e49177bbcac0fcb3cbbfd4c543da09c0f
izharabbasi <izharabbasi101@gmail.com>
2020-12-27 00:42:32 +0500

    add card

--- comments/src/index.js
+++ comments/src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 const App = () => {
-    const buttonText = {text : "Click me!"}
-    const labelText = "Enter Name : "
-
+    
     return (
-        <div>
-            <label htmlFor="input">
-                {labelText} 
-            </label>
-            <input id="input" type="text" />
-            <button style={{ backgroundColor : 'blue' , color: 'white'}}>{buttonText.text}</button>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.people()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">
+                        Nice blog post!
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
