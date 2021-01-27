b8d0d1a9bfc1842e6c076b3952dbb3a5dc21c685
izharabbasi <izharabbasi101@gmail.com>
2021-01-27 18:12:16 +0500

    added wiki api

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import Accordion from "./components/Accordion";
 import "./App.css";
+import Search from "./components/Search";
 
-const items = [
-  {
-    title : 'What is React?',
-    content : 'React is a frontend library'
-  },
-  {
-    title : 'Why use React?',
-    content : 'React is a favorite library among enginners'
-  },
-  {
-    title : 'how do we use React?',
-    content : 'We can use react by building reuseable componenets'
-  }
-]
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a frontend library",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite library among enginners",
+//   },
+//   {
+//     title: "how do we use React?",
+//     content: "We can use react by building reuseable componenets",
+//   },
+// ];
 
 function App() {
   return (
     <div>
-      <Accordion items={items}/>
+      {/* <Accordion items={items}/> */}
+      <Search />
     </div>
   );
 }
---
+++ widgets/src/components/Search.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function Search() {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+    search();
+  }, [term]);
+
+  const renderResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            type="text"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="ui celled list">{renderResults}</div>
+    </div>
+  );
+}
+
+export default Search;
